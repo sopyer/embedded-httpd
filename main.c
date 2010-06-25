941640ff805bb2aee2d9dbc005e7a85cf1595159
dbalster <dbalster@919e8d6a-8351-ea0b-3e5e-b491ccfa7fd2>
2010-06-25 18:26:59 +0000

    refactored: reduced to one single source file

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "webserver.h"
-#include "webresponse.h"
-#include "webrequest.h"
+#include "httpd.h"
 
 void* loadfile( const char* _filename, size_t* _size )
 {
@@ -22,15 +20,15 @@ void* loadfile( const char* _filename, size_t* _size )
   return mem;
 }
 
-static void indexpage( WebResponse* R )
+static void indexpage( HttpResponse* R )
 {
   // simple response, in one piece.
   // it just answers the incoming request with one single output string
   // if content-size is zero, strlen is used to determine the content size
-  webresponse_response(R, 220, "Hello, world!", 0, 0);
+  httpresponse_response(R, 220, "Hello, world!", 0, 0);
 }
 
-static void svgpage( WebResponse* R )
+static void svgpage( HttpResponse* R )
 {
   // a chunked request. every "writef" directly writes to the outgoing socket.
   // this increases the overhead/timing of the whole response, but it doesn't require
@@ -38,9 +36,9 @@ static void svgpage( WebResponse* R )
   
   // in this case, it sends a mixed SVG/HTML document
   
-  webresponse_begin(R, 220, "Content-Type: text/xml\r\n");
+  httpresponse_begin(R, 220, "Content-Type: text/xml\r\n");
   
-  webresponse_writef(R,
+  httpresponse_writef(R,
                      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
                      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n"
                      "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
@@ -51,60 +49,60 @@ static void svgpage( WebResponse* R )
                      "<body>"
                      );
   
-  webresponse_writef(R, "<h1>%s</h1>",webresponse_location(R));
+  httpresponse_writef(R, "<h1>%s</h1>",httpresponse_location(R));
 
-  webresponse_writef(R,
+  httpresponse_writef(R,
    "<svg:svg version=\"1.1\">"
    "<svg:rect style=\"fill:#f73\" id=\"x\" width=\"300 px\" height=\"300 px\" x=\"0 px\" y=\"0 px\"/>"
    "</svg:svg> "
    );
 
-  webresponse_writef(R, "</body></html>");
+  httpresponse_writef(R, "</body></html>");
   
   // don't forget to "end" the response
-  webresponse_end(R);
+  httpresponse_end(R);
 }
 
-static void inputpage( WebResponse* R )
+static void inputpage( HttpResponse* R )
 {
   // it doesn't matter if it's a POST or GET argument; you can get it
   // by its name. if the argument is unknown, a null pointer is returned
   //
   // internally all parameters/headers have already been sorted, _get_arg()
   // is using a binary search
-  const char* name = webresponse_get_arg(R, "field1");
+  const char* name = httpresponse_get_arg(R, "field1");
   if (name==0) name = "???";
   
-  webresponse_begin(R,220,0);
+  httpresponse_begin(R,220,0);
   
   // this shows how to enumerate over all arguments and headers
   
-  webresponse_writef(R, "<h2>http headers</h2>");
-  for (size_t i=0; i<webresponse_get_n_headers(R); ++i)
+  httpresponse_writef(R, "<h2>http headers</h2>");
+  for (size_t i=0; i<httpresponse_get_n_headers(R); ++i)
   {
-    const HttpHeader* hdr = webresponse_get_header_by_index(R, i);
-    webresponse_writef(R,"<li><b>%s</b> : %s</li>",hdr->name,hdr->value);
+    const HttpHeader* hdr = httpresponse_get_header_by_index(R, i);
+    httpresponse_writef(R,"<li><b>%s</b> : %s</li>",hdr->name,hdr->value);
   }
-  webresponse_writef(R, "<h2>http parameters (POST+GET)</h2>");
-  for (size_t i=0; i<webresponse_get_n_args(R); ++i)
+  httpresponse_writef(R, "<h2>http parameters (POST+GET)</h2>");
+  for (size_t i=0; i<httpresponse_get_n_args(R); ++i)
   {
-    const HttpHeader* hdr = webresponse_get_arg_by_index(R, i);
-    webresponse_writef(R,"<li><b>%s</b> : %s</li>",hdr->name,hdr->value);
+    const HttpHeader* hdr = httpresponse_get_arg_by_index(R, i);
+    httpresponse_writef(R,"<li><b>%s</b> : %s</li>",hdr->name,hdr->value);
   }
   
-  webresponse_writef(R,"<html><body><form method=\"POST\" action=\"%s\">",webresponse_location(R));
+  httpresponse_writef(R,"<html><body><form method=\"POST\" action=\"%s\">",httpresponse_location(R));
   // yes, user input needs to be verified, but that's not the topic of this example
-  webresponse_writef(R,"<input type=\"text\" name=\"field1\" value=\"%s\"/>",name);
-  webresponse_writef(R,"<input type=\"submit\"/>");
-  webresponse_writef(R,"</form></body></form>");
+  httpresponse_writef(R,"<input type=\"text\" name=\"field1\" value=\"%s\"/>",name);
+  httpresponse_writef(R,"<input type=\"submit\"/>");
+  httpresponse_writef(R,"</form></body></form>");
   
-  webresponse_end(R);
+  httpresponse_end(R);
 }
 
-static void http_handler( WebResponse* R, void* _userdata )
+static void http_handler( HttpResponse* R, void* _userdata )
 {
   // normally you would use a hashtable, map or something similar here
-  const char* loc = webresponse_location(R);
+  const char* loc = httpresponse_location(R);
   if (0==strcmp(loc,"/")) indexpage(R);
   else if (0==strcmp(loc,"/svg")) svgpage(R);
   else if (0==strcmp(loc,"/input")) inputpage(R);
@@ -112,14 +110,14 @@ static void http_handler( WebResponse* R, void* _userdata )
   {
     void* mem;
     size_t size;
-    mem = loadfile(webresponse_location(R)+1,&size);
+    mem = loadfile(httpresponse_location(R)+1,&size);
     if (mem)
     {
-      webresponse_response(R, 220, (const char*) mem, size, "Content-Type: text/xml\r\n");
+      httpresponse_response(R, 220, (const char*) mem, size, "Content-Type: text/xml\r\n");
     }
     else
     {
-      webresponse_response(R, 404, "<h1>not really found</h1>", 0, 0);
+      httpresponse_response(R, 404, "<h1>not really found</h1>", 0, 0);
     }
   }
 
@@ -128,18 +126,18 @@ static void http_handler( WebResponse* R, void* _userdata )
 static void download_something()
 {
   // important: the buffer is re-used all over again and prevents allocations
-  WebRequest* req = webrequest_create("localhost", 80, "/foo/bar/filename.zip", "GET", 256*1024);
+  HttpRequest* req = httprequest_create("localhost", 80, "/foo/bar/filename.zip", "GET", 256*1024);
   if (req)
   {
-    webrequest_execute(req);
-    const char* content = webrequest_get_content(req);
-    size_t size = strtoul(webrequest_get_header(req,"Content-Length:"),0,0);
+    httprequest_execute(req);
+    const char* content = httprequest_get_content(req);
+    size_t size = strtoul(httprequest_get_header(req,"Content-Length:"),0,0);
     if (content && size)
     {
       // OK
     }
     // frees the buffer, closes the connection
-    webrequest_destroy(req);
+    httprequest_destroy(req);
   }
 }
 
@@ -157,15 +155,15 @@ int main (int argc, const char * argv[])
 
   printf("server runs on http://localhost:8080/\n(default port 80 requires admin rights)\n");
   
-  WebServer* srv = webserver_create(8080, http_handler, 0);
+  Httpd* srv = httpd_create(8080, http_handler, 0);
   if (srv)
   {
     while (1)
     {
-      // webserver_process can be used in polling or waiting mode
-      webserver_process(srv, true);
+      // httpd_process can be used in polling or waiting mode
+      httpd_process(srv, true);
     }
-    webserver_destroy(srv);
+    httpd_destroy(srv);
   }
 
   return 0;
